879873d1cac74ec4014781479b7b08db43cccfee
Ricky Afdita Adib Priliandana <23april96@gmail.com>
2018-04-08 16:03:54 +0700

    optimize & fix layout

--- app/src/main/java/com/dytra/quakerx/GempaAdapter.java
+++ app/src/main/java/com/dytra/quakerx/GempaAdapter.java
@@ -1,8 +1,7 @@
 package com.dytra.quakerx;
 
-import java.util.List;
-
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,97 +10,33 @@ import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.List;
 
-
-
-/*
- * Custom Adapter class that is responsible for holding the list of sites after they
- * get parsed out of XML and building row views to display them on the screen.
- */
 public class GempaAdapter extends ArrayAdapter<Gempa> {
 
-	
-	public GempaAdapter(Context ctx, int textViewResourceId, List<Gempa> sites) {
-		super(ctx, textViewResourceId, sites);
-		
+    GempaAdapter(Context ctx, int textViewResourceId, List<Gempa> sites) {
+        super(ctx, textViewResourceId, sites);
+    }
 
-
-
-	}
-	
-	
-	/*
-	 * (non-Javadoc)
-	 * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
-	 * 
-	 * This method is responsible for creating row views out of a Gempa object that can be put
-	 * into our ListView
-	 */
-	@Override
-	public View getView(int pos, View convertView, ViewGroup parent){
-		RelativeLayout row = (RelativeLayout)convertView;
-		Log.i("StackSites", "getView pos = " + pos);
-		if(null == row){
-			//No recycled View, we have to inflate one.
-			LayoutInflater inflater = (LayoutInflater)parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			row = (RelativeLayout)inflater.inflate(R.layout.row_site, null);
-		}
-		
-		//Get our View References
-//		final ImageView iconImg = (ImageView)row.findViewById(R.id.iconImg);
-		TextView magnitudeTxt = (TextView)row.findViewById(R.id.magnitudeTxt);
-		TextView kedalamanTxt = (TextView)row.findViewById(R.id.kedalamanTxt);
-		TextView tanggalTxt = (TextView)row.findViewById(R.id.tanggalTxt);
-//		final ProgressBar indicator = (ProgressBar)row.findViewById(R.id.progress);
-//
-//		//Initially we want the progress indicator visible, and the image invisible
-//		indicator.setVisibility(View.VISIBLE);
-//		iconImg.setVisibility(View.INVISIBLE);
-
-		//Setup a listener we can use to swtich from the loading indicator to the Image once it's ready
-//		ImageLoadingListener listener = new ImageLoadingListener(){
-//
-//
-//
-//			@Override
-//			public void onLoadingStarted(String arg0, View arg1) {
-//				// TODO Auto-generated method stub
-//
-//			}
-//
-//			@Override
-//			public void onLoadingCancelled(String arg0, View arg1) {
-//				// TODO Auto-generated method stub
-//
-//			}
-//
-//			@Override
-//			public void onLoadingComplete(String arg0, View arg1, Bitmap arg2) {
-////				indicator.setVisibility(View.INVISIBLE);
-////				iconImg.setVisibility(View.VISIBLE);
-//			}
-//
-//			@Override
-//			public void onLoadingFailed(String arg0, View arg1, FailReason arg2) {
-//				// TODO Auto-generated method stub
-//
-//			}
-//
-//		};
-		
-		//Load the image and use our options so caching is handled.
-//		imageLoader.displayImage(getItem(pos).getImgUrl(), iconImg,options, listener);
-//
-//		//Set the relavent text in our TextViews
-		magnitudeTxt.setText(getItem(pos).getMagnitude());
-		kedalamanTxt.setText(getItem(pos).getKedalaman());
-		tanggalTxt.setText(getItem(pos).getTanggal());
-//
-		
-		
-		return row;
-				
-				
-	}
-
+    @NonNull
+    @Override
+    public View getView(int pos, View convertView, @NonNull ViewGroup parent) {
+        RelativeLayout row = (RelativeLayout) convertView;
+        Log.i("StackSites", "getView pos = " + pos);
+        if (row == null) {
+            LayoutInflater inflater = (LayoutInflater) parent
+                    .getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = (RelativeLayout) inflater.inflate(R.layout.row_site, null);
+        }
+        TextView magnitudeText = row.findViewById(R.id.magnitudeText);
+        TextView kedalamanText = row.findViewById(R.id.kedalamanText);
+        TextView wilayahText = row.findViewById(R.id.wilayahText);
+        TextView tanggalText = row.findViewById(R.id.tanggalText);
+        magnitudeText.setText(getItem(pos).getMagnitude());
+        kedalamanText.setText(getItem(pos).getKedalaman());
+        wilayahText.setText(getItem(pos).getWilayah());
+        tanggalText.setText(getItem(pos).getTanggal() + " " + getItem(pos).getJam());
+        return row;
+    }
 }
--- app/src/main/java/com/dytra/quakerx/MainActivity.java
+++ app/src/main/java/com/dytra/quakerx/MainActivity.java
@@ -1,7 +1,5 @@
 package com.dytra.quakerx;
 
-import java.io.FileNotFoundException;
-
 import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -9,69 +7,62 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ListView;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends Activity {
 
-	private GempaAdapter xAdapter;
-	private ListView sitesList;
-	private View view;
+    private GempaAdapter xAdapter;
+    private ListView sitesList;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("StackSites", "OnCreate()");
+        setContentView(R.layout.activity_main);
+        sitesList = findViewById(R.id.sitesList);
+        if (isNetworkAvailable()) {
+            Log.i("StackSites", "starting download Task");
+            SitesDownloadTask download = new SitesDownloadTask();
+            download.execute();
+        } else {
+            xAdapter = new GempaAdapter(getApplicationContext(), -1, SitesXmlPullParser.getStackSitesFromFile(MainActivity.this));
+            sitesList.setAdapter(xAdapter);
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.i("StackSites", "OnCreate()");
-		setContentView(R.layout.activity_main);
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
-		//refference
-		sitesList = (ListView) findViewById(R.id.sitesList);
+    private class SitesDownloadTask extends AsyncTask<Void, Void, Void> {
 
-		//cek koneksi
-		if(isNetworkAvailable()){
-			Log.i("StackSites", "starting download Task");
-			SitesDownloadTask download = new SitesDownloadTask();
-			download.execute();
-		}else{
-			xAdapter = new GempaAdapter(getApplicationContext(), -1, SitesXmlPullParser.getStackSitesFromFile(MainActivity.this));
-			sitesList.setAdapter(xAdapter);
-		}
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                Downloader.DownloadFromUrl(
+                        "http://data.bmkg.go.id/gempaterkini.xml",
+                        openFileOutput("datagempa.xml", Context.MODE_PRIVATE)
+                );
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-	}
-
-	//Helper method to determine if Internet connection is available.
-	private boolean isNetworkAvailable() {
-		ConnectivityManager connectivityManager
-				= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-	}
-
-	/*
-     * AsyncTask that will download the xml file for us and store it locally.
-     * After the download is done we'll parse the local file.
-     */
-	private class SitesDownloadTask extends AsyncTask<Void, Void, Void>{
-
-		@Override
-		protected Void doInBackground(Void... arg0) {
-			//Download the file
-			try {
-				Downloader.DownloadFromUrl("http://data.bmkg.go.id/gempaterkini.xml", openFileOutput("datagempa.xml", Context.MODE_PRIVATE));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result){
-			//setup our Adapter and set it to the ListView.
-			xAdapter = new GempaAdapter(MainActivity.this, -1, SitesXmlPullParser.getStackSitesFromFile(MainActivity.this));
-			sitesList.setAdapter(xAdapter);
-			Log.i("StackSites", "adapter size = "+ xAdapter.getCount());
-		}
-	}
+        @Override
+        protected void onPostExecute(Void result) {
+            xAdapter = new GempaAdapter(
+                    MainActivity.this,
+                    -1,
+                    SitesXmlPullParser.getStackSitesFromFile(MainActivity.this)
+            );
+            sitesList.setAdapter(xAdapter);
+            Log.i("StackSites", "adapter size = " + xAdapter.getCount());
+        }
+    }
 }
